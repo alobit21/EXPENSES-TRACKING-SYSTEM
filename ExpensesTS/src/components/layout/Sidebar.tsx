b95167bc030332982bb1f3898697fcda9050a007
alobit21 <macmtavangu@gmail.com>
2025-09-16 16:22:41 +0300

    build

--- ExpensesTS/src/components/layout/Sidebar.tsx
+++ ExpensesTS/src/components/layout/Sidebar.tsx
@@ -27,11 +27,17 @@ export default function Sidebar({ children }: { children: ReactNode }) {
     <aside className="h-screen">
       <nav className="h-full flex flex-col bg-white border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
- <img
-  src="/assets/logo.svg" // points to public/assets/logo.svg
-  className={`overflow-hidden transition-all ${expanded ? "w-32" : "w-0"}`}
+<img
+  src="/assets/logo.svg"
+  className={`
+    overflow-hidden transition-all
+    ${expanded ? "w-32 sm:w-40 md:w-48" : "w-0"}
+    h-auto
+    max-h-12 sm:max-h-16 md:max-h-20
+  `}
   alt="Logo"
 />
+
 
 
           <button
