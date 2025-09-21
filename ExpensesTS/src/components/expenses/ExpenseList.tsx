46d5fcb41bc879c8c8a345db69431cbed9c48797
alobit21 <macmtavangu@gmail.com>
2025-09-22 00:51:20 +0300

    rewq

--- ExpensesTS/src/components/expenses/ExpenseList.tsx
+++ ExpensesTS/src/components/expenses/ExpenseList.tsx
@@ -264,7 +264,7 @@ const ExpenseList: React.FC = () => {
                 {getPaymentMethodIcon(expense.paymentMethod)}
                 <span className="text-xs text-gray-600">{getPaymentMethodText(expense.paymentMethod)}</span>
               </div>
-              <div className="flex gap-2 flex-col sm:flex-row">
+              <div className="flex gap-2 flex-col ">
                 <button
                   onClick={() => setEditingExpense(expense)}
                   className="text-blue-600 hover:text-blue-800 p-1"
