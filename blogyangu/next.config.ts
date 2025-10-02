5ac0e8718c771c4c45194222ec4424fe883a64ce
alobit21 <macmtavangu@gmail.com>
2025-10-02 22:19:05 +0300

    changes

--- blogyangu/next.config.ts
+++ blogyangu/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/blogyangu",
+  assetPrefix: "/blogyangu",
   trailingSlash: true, // ensures URLs end with a slash
 };
 export default nextConfig;
