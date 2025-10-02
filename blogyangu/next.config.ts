5bda4b8c0640cea0bada82fea2a77a5bf1d6d517
alobit21 <macmtavangu@gmail.com>
2025-10-02 12:42:56 +0300

    changes

--- blogyangu/next.config.ts
+++ blogyangu/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/blogyangu',
+  assetPrefix: '/blogyangu',
 };
 
 export default nextConfig;
