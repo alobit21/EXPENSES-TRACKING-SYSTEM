081e314d361d3e8667c21a69a1440429640c9685
alobit21 <macmtavangu@gmail.com>
2025-10-06 16:28:56 +0300

    fixing

--- blogyangu/prisma/migrations/20250929081841_comments/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "public"."CommentStatus" ADD VALUE 'REJECTED';
-
--- AlterTable
-ALTER TABLE "public"."comments" ADD COLUMN     "deletedAt" TIMESTAMP(3),
-ADD COLUMN     "moderationNote" TEXT;
